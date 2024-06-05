19e3c41be883325e175f43ca5d5a8f9499991e25
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-06-05 01:30:11 -0300

    validating input

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,9 @@
-console.log('teste');
+const amount = document.getElementById('amount') as HTMLInputElement;
+
+// making input amount to receive only numbers:
+if (amount) {
+  amount.addEventListener('input', () => {
+    const hasCharactersRegex = /\D+/g;
+    amount.value = amount.value.replace(hasCharactersRegex, '');
+  });
+}
