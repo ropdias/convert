d0d96ef28a4d2d600d05675c2af22fdf0475fce8
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-06-05 21:02:52 -0300

    getting the selected coin

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,9 +1,18 @@
+const form = document.querySelector('form') as HTMLFormElement;
 const amount = document.getElementById('amount') as HTMLInputElement;
+const currency = document.getElementById('currency') as HTMLSelectElement;
 
-// making input amount to receive only numbers:
-if (amount) {
+if (form && amount && currency) {
+  // making input amount to receive only numbers:
   amount.addEventListener('input', () => {
     const hasCharactersRegex = /\D+/g;
     amount.value = amount.value.replace(hasCharactersRegex, '');
   });
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    console.log(currency.value);
+  };
+} else {
+  console.error('Um ou mais elementos não foram encontrados.');
 }
