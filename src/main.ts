0a0cdbfe443afaa93bb498a2fe3a3f0bf8d0cfe0
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-12-17 00:15:10 -0300

    add prettier, eslint config and update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,62 @@
 // Currency price of the day
-const USD = 4.87;
-const EUR = 5.32;
-const GBP = 6.08;
+const USD = 6.15
+const EUR = 6.47
+const GBP = 7.8
 
 // Getting form elements:
-const form = document.querySelector('form') as HTMLFormElement;
-const amount = document.getElementById('amount') as HTMLInputElement;
-const currency = document.getElementById('currency') as HTMLSelectElement;
-const footer = document.querySelector('main footer') as HTMLElement;
-const description = document.getElementById('description') as HTMLSpanElement;
-const result = document.getElementById('result') as HTMLHeadingElement;
+const form = document.querySelector('form') as HTMLFormElement
+const amount = document.getElementById('amount') as HTMLInputElement
+const currency = document.getElementById('currency') as HTMLSelectElement
+const footer = document.querySelector('main footer') as HTMLElement
+const description = document.getElementById('description') as HTMLSpanElement
+const result = document.getElementById('result') as HTMLHeadingElement
 
 if (form && amount && currency) {
   // making input amount to receive only numbers:
   amount.addEventListener('input', () => {
-    const hasCharactersRegex = /\D+/g;
-    amount.value = amount.value.replace(hasCharactersRegex, '');
-  });
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, '')
+  })
 
   form.onsubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     switch (currency.value) {
       case 'USD':
-        convertCurrency(amount.value, USD, 'US$');
-        break;
+        convertCurrency(amount.value, USD, 'US$')
+        break
       case 'EUR':
-        convertCurrency(amount.value, EUR, '€');
-        break;
+        convertCurrency(amount.value, EUR, '€')
+        break
       case 'GBP':
-        convertCurrency(amount.value, GBP, '£');
-        break;
+        convertCurrency(amount.value, GBP, '£')
+        break
     }
-  };
+  }
 } else {
-  console.error('Um ou mais elementos não foram encontrados.');
+  console.error('Um ou mais elementos não foram encontrados.')
 }
 
 function convertCurrency(amount: string, price: number, symbol: string) {
   try {
     // Showing selected currency price
-    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
-    let total = Number(amount) * price;
+    const total = Number(amount) * price
 
     if (isNaN(total)) {
-      return alert('Por favor, digite o valor corretamente para converter.');
+      return alert('Por favor, digite o valor corretamente para converter.')
     }
 
-    result.textContent = `${formatCurrencyBRL(total).replace('R$', '')} Reais`;
+    result.textContent = `${formatCurrencyBRL(total).replace('R$', '')} Reais`
 
     // Add the class that shows footer
-    footer.classList.add('show-result');
+    footer.classList.add('show-result')
   } catch (error) {
     // Remove the class that shows footer
-    footer.classList.remove('show-result');
-    console.log(error);
-    alert('Não foi possível converter. Tente novamente mais tarde.');
+    footer.classList.remove('show-result')
+    console.log(error)
+    alert('Não foi possível converter. Tente novamente mais tarde.')
   }
 }
 
@@ -64,5 +64,5 @@ function formatCurrencyBRL(value: number): string {
   return value.toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-  });
+  })
 }
